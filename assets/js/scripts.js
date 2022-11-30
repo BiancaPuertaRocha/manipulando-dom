0be6dfc36f58948daacf628a317c6b8ce3f522af
BiancaPuertaRocha <biapuertarocha@gmail.com>
2022-11-30 14:19:03 -0300

    fix prints and undefined

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,11 @@
 function toggle_dark_mode(){
     var elements_list = ["body", "footer", "button"]
     for(var i in elements_list){
-        console.log(elements_list[i])
         var elements = document.getElementsByTagName(elements_list[i])
-        console.log(elements)
         for (var k in elements){
-            elements[k].classList.toggle("dark-mode");
+            var classList = elements[k].classList
+            if(classList != undefined)
+                classList.toggle("dark-mode");
         }
     }
     
